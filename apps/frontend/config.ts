357cf3af02dd4f20704ec66b0258a8fdbf6c7e58
aniket4518 <jhaaniket2005@gmail.com>
2026-03-03 15:54:05 +0530

    fully deployed

--- apps/frontend/config.ts
+++ apps/frontend/config.ts
@@ -1,2 +1,2 @@
-export const  Http_Backend = "http://localhost:3001";
-export const ws_backend =  "ws://localhost:8080"
+export const Http_Backend = process.env.NEXT_PUBLIC_HTTP_BACKEND_URL!;
+export const ws_backend = process.env.NEXT_PUBLIC_WS_BACKEND_URL!;
